e30c0d22093443116b80d889ddd2e0742a09cb75
NghiemNgocDuc <161023401+NghiemNgocDuc@users.noreply.github.com>
2025-03-12 19:20:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 def get_book_recommendations(query):
-    api_key = 'GET https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=AIzaSyB4Hd1CQSBRdEOd1r4CTlz5Sg2wwzYEjKE'  # Replace with your API key
+    api_key = 'GET https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=your key'  # Replace with your API key
     url = f'https://www.googleapis.com/books/v1/volumes?q={query}&key={api_key}'
     response = requests.get(url)
     if response.status_code == 200:
